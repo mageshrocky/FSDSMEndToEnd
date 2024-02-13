03b810f6b326707091b5bbc12827979b457ac0e0
mageshrocky <magesh1699@gmail.com>
2024-02-13 12:18:11 +0530

    code added in components and utils

--- src/DiamondPricePrediction/logger.py
+++ src/DiamondPricePrediction/logger.py
@@ -14,5 +14,5 @@ logging.basicConfig(level=logging.INFO,
                     filename=LOG_FILEPATH,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.info("Here again i'm testing")
